3438f94791e7a7a9d71fc70215f9deca5f122e1e
Edwin Kwok <edwin.kwok@accenture.com>
2013-05-13 22:52:38 -0700

    Replaced Zurb foundation orbit with slider.

--- js/featured-title.js
+++ js/featured-title.js
@@ -2,71 +2,64 @@
  * View logic for featured title page.
  */
 
-"use strict";
-
 var featuredTitleView = (function() {
-	var $carousel = $(".mdmc-featured-title.mdmc-carousel"),
-		$orbit = $carousel.children("ul[data-orbit]"),
-		setSliderHeight = function() {
-			var imageWidth = $(".mdmc-featured-title.mdmc-slider section .mdmc-slider-container .slider .item img").width(),
-				ratio = 242 / 304;
-			
-			$(".mdmc-featured-title.mdmc-slider section").css("height", + (imageWidth / ratio) + "px");
-			$(".mdmc-featured-title.mdmc-slider section .mdmc-slider-container").css("height", + (imageWidth / ratio) + "px");
-			$(".mdmc-featured-title.mdmc-slider section .mdmc-slider-container .slider").css("height", + (imageWidth / ratio) + "px");
-			$(".mdmc-featured-title.mdmc-slider section .mdmc-slider-container .slider .item img").css("height", + (imageWidth / ratio) + "px");
-		},
-		delay = (function(){
-			var timer = 0;
-			
-			return function(callback, ms) {
-				clearTimeout(timer);
-				timer = setTimeout(callback, ms);
-			}
-		})();
+	"use strict";
+	
+	var dataTimer = 0,
+		dataBullets = false,
+		$topbarCtn = $(".mdmc-header .contain-to-grid.sticky"),
+		$carouselCtn = $(".mdmc-featured-title.mdmc-carousel .mdmc-slider-container"),
+		$carousel = $carouselCtn.children(".slider"),
+		$sliderCtn = $(".mdmc-featured-title.mdmc-slider .mdmc-slider-container"),
+		$slider = $sliderCtn.children(".slider"),
+		$promos = $(".mdmc-featured-title.mdmc-promos section"),
+		
+		onSlideItemChange = function(args) {
+			$(".selectors .item").removeClass("selected");
+			$(".selectors .item:eq(" + (args.currentSlideNumber - 1) + ")").addClass("selected");
+		};
 	
 	var FeaturedTitleView = function() {
-		if($carousel.length > 0) {
-			$carousel.foundation("orbit", function(response) {
-				if(response.errors.length === 0) {
-					if($orbit.length <= 0) {
-						console.log("FeaturedTitleView: Cannot find ul[data-orbit]!");
-					}
-				}
-				else {
-					console.log("FeaturedTitleView: Failed to initialize orbit!");
+		if($topbarCtn.length > 0) {
+			$topbarCtn.foundation("topbar", function(response) {
+				if(response.errors.length !== 0) {
+					console.log("FeaturedTitleView: Failed to initialize topbar!");
 				}
 			});
 		}
 		else {
-			console.log("FeaturedTitleView: Cannot find .mdmc-featured-title.mdmc-carousel!");
+			console.log("FeaturedTitleView: Cannot find $topbarCtn!");
 		}
+		
+		dataTimer = parseInt($carouselCtn.attr("data-timer") || 0);
+		dataBullets = $carouselCtn.attr("data-bullets") || false;
 	}
 	
 	FeaturedTitleView.prototype.init = function() {
-		setSliderHeight();
-		
-		$(window).resize(function() {
-			delay(setSliderHeight, 50);
+		$carouselCtn.iosSlider({
+			snapToChildren: true,
+			desktopClickDrag: true,
+			navSlideSelector: $(".selectors .item"),
+			onSlideChange: onSlideItemChange,
+			infiniteSlider: true,
+			autoSlide: dataTimer > 0,
+			autoSlideTimer: (dataTimer > 0)? dataTimer: 0
 		});
 		
-		$orbit.on("click", "li.active>div, li.active>div>img", function(e) {
-			Foundation.libs.orbit._stop_timer($orbit);
-			
-			e.stopPropagation();
-			//e.stopImmediatePropagation();
+		$sliderCtn.iosSlider({
+			snapToChildren: true,
+			desktopClickDrag: true,
+		});
+
+		$carousel.add($slider).add($promos).on("click", "div, div>img", function(e) {
+			//e.stopPropagation();
 			
 			if(e.type === "click") {
 				if(e.currentTarget.nodeName === "IMG") {
-					var catentryid = $(e.target).parents("li").attr("data-catentryid");
+					var catentryid = $(e.target).parents("div.item").attr("data-catentryid");
 					alert(e.type + ": " + catentryid);
 				}
 			}
-		});
-		
-		$(".mdmc-featured-title.mdmc-slider .mdmc-slider-container").iosSlider({
-			snapToChildren: true,
-			desktopClickDrag: true,
 		});
 	}
 	
